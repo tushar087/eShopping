2b10cecb2bc9ac13d00c05b5250cd8a085a26ed2
Tushar <tushar.beniwal@outlook.com>
2022-12-30 02:05:24 +0530

    commit for 30-12-2022

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -10,9 +11,10 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-
-        public BasketController(IBasketRepository repository)
+        private readonly DiscountGrpcService _discountGrpcService;
+        public BasketController(IBasketRepository repository,DiscountGrpcService discountGrpcService)
         {
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
             _repository=repository?? throw new ArgumentNullException(nameof(repository));
         }
 
@@ -28,6 +30,11 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            foreach(var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName==null?"":item.ProductName);
+                item.Price -= coupon.Amount;
+            }
             
             return Ok(await _repository.UpdateBasket(basket));
         }
