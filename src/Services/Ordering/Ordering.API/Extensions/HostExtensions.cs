b4b8090dac25ba7da6009740c12e1e26b2643ffd
Tushar <tushar.beniwal@outlook.com>
2023-02-05 02:53:57 +0530

    commit

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+           public static IHost MigrateDatabase<TContext>(this IHost host,Action<TContext,IServiceProvider> seeder,int? retry=0) where TContext :DbContext
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger=services.GetRequiredService<ILogger<TContext>>();
+                var context=services.GetRequiredService<TContext>();
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(DbContext).Name);
+
+                    InvokeSeeder(seeder, context, services);
+
+                    logger.LogInformation("Migrate database associated with context {DbContextName}", typeof(DbContext).Name);
+                }
+                catch(SqlException ex)
+                {
+                    logger.LogError(ex,"An error occurred while migrating the database used on context {DbContextName}",typeof(DbContext).Name);
+
+                    if(retryForAvailability<50)
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                    }
+                }
+            }
+
+
+            return host;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext,IServiceProvider> seeder,TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext,ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfigureOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfigureOrders()
+        {
+            return new List<Order>
+            {
+                new Order(){UserName="tbeniwal",FirstName="Tushar",LastName="Beniwal",EmailAddress="tbeniwal@gmail.com",AddressLine="Jodhpur",Country="India",TotalPrice=400}
+            };
+        }
+    }
+}
