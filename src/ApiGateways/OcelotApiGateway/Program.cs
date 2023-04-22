5d5bce1500d103ba9123f322e54b39df1c3d17d9
tushar087 <tushar087@gmail.com>
2023-04-23 01:39:05 +0530

    commit on 23-04-2023

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,6 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-
+using Ocelot.Cache.CacheManager;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
@@ -9,7 +9,11 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().
+    AddCacheManager(x =>
+    {
+        x.WithDictionaryHandle();
+    });
 var app = builder.Build();
 
 
