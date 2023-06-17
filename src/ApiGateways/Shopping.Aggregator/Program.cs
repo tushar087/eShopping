3d5dc28e9a20ebf4204b2a5f0a5f7290a374bd4f
tushar087 <tushar087@gmail.com>
2023-06-18 02:47:29 +0530

    commit for shopping aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -0,0 +1,41 @@
+using Shopping.Aggregator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddHttpClient<ICatalogService,CatalogService>(c=>c.BaseAddress
+=new Uri(builder.Configuration["ApiSettings:CatalogUrl"]));
+
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress
+= new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress
+= new Uri(builder.Configuration["ApiSettings:OrderingUrl"]));
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo { Title="Shopping Aggregator", Version="v1"});
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
