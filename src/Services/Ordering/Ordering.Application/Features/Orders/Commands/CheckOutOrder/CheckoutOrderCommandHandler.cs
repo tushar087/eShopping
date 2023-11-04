ef78d496ca30ef3440f6767d1ba31cf7fe239582
tushar087 <tushar087@gmail.com>
2023-11-04 14:15:15 +0530

    commit for correct mapping of UpdateOrderCommand, CheckOutOrderCommand with Order Class

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckOutOrder/CheckoutOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckOutOrder/CheckoutOrderCommandHandler.cs
@@ -1,9 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
-using Ordering.Application.Models;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Features.Orders.Commands.CheckOutOrder
@@ -12,14 +10,12 @@ namespace Ordering.Application.Features.Orders.Commands.CheckOutOrder
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IMapper _mapper;
-     //   private readonly IEmailService _emailService;
         private readonly ILogger<CheckoutOrderCommandHandler> _logger;
 
         public CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<CheckoutOrderCommandHandler> logger)
         {
             _orderRepository = orderRepository?? throw new ArgumentNullException(nameof(orderRepository));
             _mapper = mapper?? throw new ArgumentNullException(nameof(mapper));
-           // _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -31,23 +27,10 @@ namespace Ordering.Application.Features.Orders.Commands.CheckOutOrder
 
             _logger.LogInformation($"Order {newOrder.Id} is successfully created");
 
-           // await SendEmail(newOrder);
 
             return newOrder.Id;
         }
 
-        //private async Task SendEmail(Order order)
-        //{
-        //    var email = new Email() { To = "tushar087@gmail.com", Body = $"Order was created", Subject = $"Order was created" };
-
-        //    try
-        //    {
-        //        await _emailService.SendEmail(email);
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        _logger.LogError($"Order {order.Id} failed due to error in mail service: {ex.Message}");
-        //    }
-        //}
+       
     }
 }
--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -8,31 +8,31 @@ namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
     {
 
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
 
-        public decimal TotalPrice { get; set; }
+        public decimal? TotalPrice { get; set; }
 
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
-        public string EmailAddress { get; set; }
+        public string? EmailAddress { get; set; }
 
-        public string AddressLine { get; set; }
+        public string? AddressLine { get; set; }
 
-        public string Country { get; set; }
+        public string? Country { get; set; }
 
-        public string State { get; set; }
+        public string? State { get; set; }
 
-        public string ZipCode { get; set; }
+        public string? ZipCode { get; set; }
 
-        public string CardName { get; set; }
-        public string CardNumber { get; set; }
+        public string? CardName { get; set; }
+        public string? CardNumber { get; set; }
 
-        public string Expiration { get; set; }
+        public string? Expiration { get; set; }
 
-        public string CVV { get; set; }
+        public string? CW { get; set; }
 
-        public int PaymentMethod { get; set; }
+        public int? PaymentMethod { get; set; }
     }
 }
