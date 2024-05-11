64556c502873f4f5f7c38b10d2b3099fa1259f27
tushar087 <tushar087@gmail.com>
2024-05-12 00:36:19 +0530

    angular client first commit

---
+++ src/angularclient/src/shared/models/basket.ts
@@ -0,0 +1,23 @@
+export interface IBasketItem{
+    quantity :number;
+    imageFile:string;
+    price:number;
+    productId:number;
+    productName:string;
+}
+
+export interface IBasket{
+   userName: string;
+   items: IBasketItem[];
+   totalPrice : number;
+}
+
+export class Basket implements IBasket{
+    userName: string ='tushar';
+    items: IBasketItem[] = [];
+    totalPrice : number=0;
+}
+
+export interface IBasketTotal{
+    total: number;
+}
